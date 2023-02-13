9468ec9a035753ade86dfb55b18013b18dc377b4
Francis <fxs@fsullivans.net>
2023-02-13 11:52:20 -0700

    update SERP tests

--- spec/boxcars/serp_spec.rb
+++ spec/boxcars/serp_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe Boxcars::Serp do
   context "without a serpapi api key" do
     it "raises an error" do
       expect do
-        Boxcars::Serp.new(serpapi_api_key: nil).run("what temperature is it in Austin?")
+        described_class.new(serpapi_api_key: nil).run("what temperature is it in Austin?")
       end.to raise_error(Boxcars::ConfigurationError)
+    end
+
+    it "gets the temperature in Austin" do
+      VCR.use_cassette("serp") do
+        expect(described_class.new.run("what temperature is it in Austin TX right now?")).to eq("50 °F")
+      end
     end
   end
 end
