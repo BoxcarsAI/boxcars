afa53edd04d50b0e5b8a06dde15816305a79570e
Peter Goldstein <peter.m.goldstein@gmail.com>
2023-02-16 13:09:50 -0500

    Stubs out environment variables for keys if they are not already presents in the environment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,18 +30,11 @@ RSpec.configure do |c|
   c.expect_with :rspec do |rspec|
     rspec.syntax = :expect
   end
+
+  c.before do
+    allow(ENV).to receive(:fetch).with("OPENAI_ACCESS_TOKEN", nil).and_return("abcdef") unless ENV["OPENAI_ACCESS_TOKEN"]
+    allow(ENV).to receive(:fetch).with("SERPAPI_API_KEY", nil).and_return("abcdef") unless ENV["SERPAPI_API_KEY"]
+  end
 end
 
 RSPEC_ROOT = File.dirname __FILE__
-
-# RSpec.configure do |config|
-#   # Enable flags like --only-failures and --next-failure
-#   config.example_status_persistence_file_path = ".rspec_status"
-
-#   # Disable RSpec exposing methods globally on `Module` and `main`
-#   config.disable_monkey_patching!
-
-#   config.expect_with :rspec do |c|
-#     c.syntax = :expect
-#   end
-# end
