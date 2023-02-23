d49330417e743d6816c18b47fab7d860ebd16bf8
Alexander Kovtunov <1sasha.sasha1@gmail.com>
2023-02-23 20:10:00 +0100

    [fix] Fixed specs which required a key - moved it inside the test

--- spec/boxcars/zero_shot_spec.rb
+++ spec/boxcars/zero_shot_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe Boxcars::ZeroShot do
   let(:engine) { Boxcars::Openai.new }
   let(:search) { Boxcars::GoogleSearch.new }
   let(:calculator) { Boxcars::Calculator.new(engine: engine) }
-  let(:train) { described_class.new(engine: engine, boxcars: [search, calculator]) }
 
   it "can execute a train" do
+    train = described_class.new(engine: engine, boxcars: [search, calculator])
+
     VCR.use_cassette("zeroshot") do
       question = "What is pi times the square root of the average temperature in Austin TX in January?"
       expect(train.run(question)).to eq("25.132741228718345")
@@ -14,12 +15,13 @@ RSpec.describe Boxcars::ZeroShot do
   end
 
   context "with sample helpdesk app" do
-    helpdesk = Boxcars::ActiveRecord.new(models: [Comment, Ticket, User], name: "helpdesk")
-    new_train = described_class.new(boxcars: [helpdesk, Boxcars::Calculator.new, Boxcars::GoogleSearch.new])
+    let(:helpdesk) { Boxcars::ActiveRecord.new(models: [Comment, Ticket, User], name: "helpdesk") }
+
     it "can use a train to answer a question" do
+      train = described_class.new(boxcars: [helpdesk, calculator, search])
       VCR.use_cassette("zeroshot2") do
         question = "Count the comments from John for open tickets and multiply by pi to 5 decimal places."
-        expect(new_train.run(question)).to eq("6.28319")
+        expect(train.run(question)).to eq("6.28319")
       end
     end
   end
