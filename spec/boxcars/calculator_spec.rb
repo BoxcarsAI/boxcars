3b82e4b94e2b94b5cab6457b122c9b3cd0acf608
Francis <fxs@fsullivans.net>
2023-03-14 13:39:34 -0700

    add calculator tests

--- spec/boxcars/calculator_spec.rb
+++ spec/boxcars/calculator_spec.rb
@@ -2,11 +2,24 @@
 
 RSpec.describe Boxcars::Calculator do
   context "without a serpapi api key" do
-    it "raises an error" do
-      engine = Boxcars::Openai.new
+    it "raises an error", skip_tokens: true do
+      expect do
+        described_class.new.run("1 + 1")
+      end.to raise_error(Boxcars::ConfigurationError)
+    end
+  end
+
+  context "with a serpapi api key" do
+    it "can do hard math" do
       VCR.use_cassette("calculator") do
-        expect(described_class.new(engine: engine)
+        expect(described_class.new
           .run("what is 2.173 to the power of 22.1 then diveded by 27.2 to 5 significant digits?")).to eq("1033834.56373")
+      end
+    end
+
+    it "can do easy math" do
+      VCR.use_cassette("calculator2") do
+        expect(described_class.new.run("what is 1 plus 2?")).to eq("3")
       end
     end
   end
