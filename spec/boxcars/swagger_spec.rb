663c55d5b1b52743844e6cfd8de24b6700f7ebc5
Francis <fxs@fsullivans.net>
2023-04-12 11:11:42 -0700

    add test about swagger API question

--- spec/boxcars/swagger_spec.rb
+++ spec/boxcars/swagger_spec.rb
@@ -8,14 +8,20 @@ RSpec.describe Boxcars::Swagger do
     it "can do simple API call from swagger file" do
       VCR.use_cassette("swagger") do
         expect(described_class.new
-          .run(question: "How many Sentires are there?", swagger_url: swagger_url,
-               context: "API_token: #{mc_api_token}, sentry api path: api/v1/sentries")).to eq("194")
+          .run(question: "How many Sentries are there?", swagger_url: swagger_url,
+               context: "API_token: #{mc_api_token}")).to eq("194")
       end
     end
 
     it "can do easy question" do
       VCR.use_cassette("swagger2") do
         expect(described_class.new.run(question: "what is 1 plus 2?", context: "", swagger_url: swagger_url)).to include("3")
+      end
+    end
+
+    it "can answer a question about the API" do
+      VCR.use_cassette("swagger3") do
+        expect(described_class.new.run(question: "describe the available APIs for sentries", context: "don't forget to require the yaml library", swagger_url: swagger_url)).to include("api/v1/sentries")
       end
     end
   end
