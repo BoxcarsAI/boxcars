3f711553ac56f807a338b154eba2ecaa88b9f5d8
Francis <fxs@fsullivans.net>
2023-06-09 16:07:06 -0700

    make suggested next actions optional (#94)

--- lib/boxcars/train/zero_shot.rb
+++ lib/boxcars/train/zero_shot.rb
@@ -5,22 +5,25 @@ module Boxcars
   # A Train using the zero-shot react method.
   class ZeroShot < Train
     attr_reader :boxcars, :observation_prefix, :engine_prefix
+    attr_accessor :wants_next_actions
 
     # @param boxcars [Array<Boxcars::Boxcar>] The boxcars to run.
     # @param engine [Boxcars::Engine] The engine to use for this train.
     # @param name [String] The name of the train. Defaults to 'Zero Shot'.
     # @param description [String] The description of the train. Defaults to 'Zero Shot Train'.
     # @param prompt [Boxcars::Prompt] The prompt to use. Defaults to the built-in prompt.
-    def initialize(boxcars:, engine: nil, name: 'Zero Shot', description: 'Zero Shot Train', prompt: nil)
+    # @param kwargs [Hash] Additional arguments to pass to the train. wants_next_actions: true
+    def initialize(boxcars:, engine: nil, name: 'Zero Shot', description: 'Zero Shot Train', prompt: nil, **kwargs)
       @observation_prefix = 'Observation: '
       @engine_prefix = 'Thought:'
+      @wants_next_actions = kwargs.fetch(:wants_next_actions, false)
       prompt ||= my_prompt
       super(engine: engine, boxcars: boxcars, prompt: prompt, name: name, description: description)
     end
 
     # @return Hash The additional variables for this boxcar.
     def prediction_additional(_inputs)
-      { boxcar_names: boxcar_names, boxcar_descriptions: boxcar_descriptions }.merge super
+      { boxcar_names: boxcar_names, boxcar_descriptions: boxcar_descriptions, next_actions: next_actions }.merge super
     end
 
     # Extract the boxcar and input from the engine output.
@@ -72,13 +75,14 @@ module Boxcars
            "Question: the input question you must answer\n",
            "Thought: you should always think about what to do\n",
            "Action: the action to take, should be one from this list: %<boxcar_names>s\n",
-           "Action Input: the input to the action\n",
+           "Action Input: an input question to the action\n",
            "Observation: the result of the action\n",
            "... (this Thought/Action/Action Input/Observation sequence can repeat N times)\n",
            "Thought: I know the final answer\n",
            "Final Answer: the final answer to the original input question\n",
-           "Next Actions: Up to 3 logical suggested next questions for the user to ask after getting this answer.\n",
+           "%<next_actions>s\n",
            "Remember to start a line with \"Final Answer:\" to give me the final answer.\n",
+           "Also make sure to specify a question for the Action Input.\n",
            "Begin!"),
       user("Question: %<input>s"),
       assi("Thought: %<agent_scratchpad>s")
@@ -92,13 +96,21 @@ module Boxcars
       @boxcar_descriptions ||= boxcars.map { |boxcar| "#{boxcar.name}: #{boxcar.description}" }.join("\n")
     end
 
+    def next_actions
+      if wants_next_actions
+        "Next Actions: Up to 3 logical suggested next questions for the user to ask after getting this answer.\n"
+      else
+        ""
+      end
+    end
+
     # The prompt to use for the train.
     def my_prompt
       @conversation ||= Conversation.new(lines: CTEMPLATE)
       @my_prompt ||= ConversationPrompt.new(
         conversation: @conversation,
         input_variables: [:input],
-        other_inputs: [:boxcar_names, :boxcar_descriptions, :agent_scratchpad],
+        other_inputs: [:boxcar_names, :boxcar_descriptions, :next_actions, :agent_scratchpad],
         output_variables: [:answer])
     end
   end
