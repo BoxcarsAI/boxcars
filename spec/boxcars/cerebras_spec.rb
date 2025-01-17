20eb073cc9949110359e8fde9827f9a3e7503cee
Francis <fxs@fsullivans.net>
2025-01-17 13:47:05 -0700

    update test

--- spec/boxcars/cerebras_spec.rb
+++ spec/boxcars/cerebras_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Boxcars::Cerebras do
   context "with a cerebras api key" do
     it "can write a short poem" do
       VCR.use_cassette("cerebras") do
-        expect(described_class.new.run("write a haiku about love")).to eq("Softly glowing flame\nWarming heart and tender soul\nLove's eternal kiss")
+        expect(described_class.new.run("write a haiku about love")).to eq("Softly glowing flame\nWarming heart and gentle soul\nLove's eternal kiss")
       end
     end
   end
